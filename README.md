ccea893242e6864e94fc71da48380c19f54a303a
kccnma <gargiulo@hawaii.edu>
2020-03-09 15:15:46 -1000

    upgraded gulp to version 4

--- README.md
+++ README.md
@@ -1,13 +1,45 @@
-# sitebase3-npmgulp
-NPM Gulp version of Sitebase, a minimal HTML + CSS + JS base front-end framework for simple web site projects.
+# Sitebase npm gulp version
 
-## Install
+A npm gulp version of Site base. 2020 Update: upgraded to Gulp 4 via [Gulp Boilerplate](https://github.com/kieranphilipbrown/gulp-boilerplate) by [Kieran Brown](https://github.com/kieranphilipbrown).
 
-```
-$ npm install
-```
-## Usage:
 
+## Local install instructions:
+1. Make sure you have node and npm installed. See installation docs: (https://nodejs.org/en/). To confirm node and npm are installed:
 ```
-$ gulp
+    node -v
+    npm -v
 ```
+2. Make sure you have Gulp installed. To install Gulp globally (if you don't already have it). Full installation docs: (https://gulpjs.com/docs/en/getting-started/quick-start).
+```
+    npm install gulp-cli -g
+```
+- To confirm gulp is insalled:
+```
+    gulp --version
+```
+3. Once confirmed that you have npm and gulp installed, install the project files via npm:
+```
+    npm install
+```
+* a note: you might be prompted to run npm audit
+
+## Local dev instructions:
+Run gulp watcher:
+```
+gulp
+```
+Live reload should  initiate the project on http://localhost:3000. 
+
+## Build instructions:
+
+To build:
+```
+gulp build
+```
+
+### Update log:
+* March 2020
+    * updated gulp to v4
+
+### Roadmap (future plans/goals):
+* Add support for templating (e.g. nunjucks, jekyll, liquid, etc.)
--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,88 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var cache = require('gulp-cache');
-var imagemin = require('gulp-imagemin');
+const gulp = require("gulp");
+const { parallel, series } = require("gulp");
 
-// BROWSER-SYNC
-gulp.task('sync', function() {
-    browserSync.init('', {
+const imagemin = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create(); //https://browsersync.io/docs/gulp#page-top
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+
+// /*
+// TOP LEVEL FUNCTIONS
+//     gulp.task = Define tasks
+//     gulp.src = Point to files to use
+//     gulp.dest = Points to the folder to output
+//     gulp.watch = Watch files and folders for changes
+// */
+
+// Optimise Images
+function imageMin(cb) {
+    gulp.src("src/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+    cb();
+}
+
+
+// Minify and copy HTML
+function html(cb) {
+    gulp.src("src/*.html")
+        .pipe(gulp.dest("dist"))
+        .pipe(
+            htmlmin({
+                collapseWhitespace: true
+            })
+        )
+        .pipe(gulp.dest("dist"));
+    cb();
+}
+
+// Scripts
+function js(cb) {
+    gulp.src("src/js/*js")
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(concat("script.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"));
+    cb();
+}
+
+// Compile Sass
+function css(cb) {
+    gulp.src("src/scss/*.scss")
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+        .pipe(autoprefixer({
+            browserlist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("dist/css"))
+        // Stream changes to all browsers
+        .pipe(browserSync.stream());
+    cb();
+}
+
+
+
+// Watch Files
+function watch_files() {
+    browserSync.init({
         server: {
-            baseDir: './dist/'
+            baseDir: "dist/"
         }
-    })
-})
+    });
+    gulp.watch("src/scss/**/*.scss", css);
+    gulp.watch("src/js/*.js", js).on("change", browserSync.reload);
+    gulp.watch("src/*.html", html).on("change", browserSync.reload);
 
-// HTML
-gulp.task('html', function() {
-    return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist/'))
-        .pipe(browserSync.reload({ stream: true }))
-})
+}
 
-// SASS
-gulp.task('sass', function() {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({ stream: true }))
-})
+// Default 'gulp' command with start local server and watch files for changes.
+exports.default = series(html, css, js, imageMin, watch_files);
 
-// JAVASCRIPT
-gulp.task('js', function() {
-    return gulp.src('src/js/**/*.js')
-        .pipe(gulp.dest('dist/js/'))
-        .pipe(browserSync.reload({ stream: true }))
-})
-
-// IMAGES
-gulp.task('images', function() {
-    return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(cache(imagemin()))
-        .pipe(gulp.dest('dist/img/'))
-        .pipe(browserSync.reload({ stream: true }))
-})
-
-// BUILD SITE
-gulp.task('build', ['html', 'sass', 'js', 'images']) 
-
-// WATCH
-gulp.task('watch', ['build', 'sync'], function() {
-    global.isWatching = true 
-    gulp.watch('src/**/*.html', ['html']); 
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['js']); 
-    gulp.watch('src/img/**/*.+(png|jpg|jpeg|gif|svg)', ['images']); 
-})
-
-gulp.task('default', ['watch'])
+// 'gulp build' will build all assets but not run on a local server.
+exports.build = parallel(html, css, js, imageMin);

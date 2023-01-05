47eff19a83807394a5f62489215752639d944311
KCC New Media Arts <gargiulo@hawaii.edu>
2023-01-04 16:09:09 -1000

    fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,21 @@
-"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".menu-button");for(t=0;t<e.length;t++)e[t].onclick=function(){this.classList.toggle("expanded")};var t,n=document.querySelectorAll(".accordian");for(t=0;t<n.length;t++)n[t].getElementsByTagName("h4")[0].onclick=function(){this.parentElement.classList.toggle("open")}});
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  //TOGGLE MENU NAV
+  var menubutton = document.querySelectorAll('.menu-button');
+  var i;
+  for (i = 0; i < menubutton.length; i++) {
+    menubutton[i].onclick = function () {
+      this.classList.toggle("expanded");
+    };
+  }
+
+  //TOGGLE ACCORDIAN
+  var acc = document.querySelectorAll('.accordian');
+  var i;
+  for (i = 0; i < acc.length; i++) {
+    acc[i].getElementsByTagName('h4')[0].onclick = function () {
+      this.parentElement.classList.toggle("open");
+    };
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require("gulp");
 const { parallel, series } = require("gulp");
 
-const imagemin = require("gulp-imagemin");
 const htmlmin = require("gulp-htmlmin");
+const sass = require('gulp-sass')(require('sass'));
 const uglify = require("gulp-uglify");
-const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
 const concat = require("gulp-concat");
-const browserSync = require("browser-sync").create(); //https://browsersync.io/docs/gulp#page-top
+const browserSync = require("browser-sync").create(); //https://browsersync.io/docs/gulp
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
+const rename = require("gulp-rename");
+
 
 // /*
 // TOP LEVEL FUNCTIONS
@@ -18,44 +20,24 @@ const babel = require('gulp-babel');
 //     gulp.watch = Watch files and folders for changes
 // */
 
-// Optimise Images
-function imageMin(cb) {
-    gulp.src("src/img/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"));
-    cb();
-}
 
-
-// Minify and copy HTML
+// HTML
 function html(cb) {
-    gulp.src("src/*.html")
+    gulp.src("src/**/*.html")
         .pipe(gulp.dest("dist"))
         .pipe(
             htmlmin({
-                collapseWhitespace: true
+                collapseWhitespace: false
             })
         )
         .pipe(gulp.dest("dist"));
     cb();
 }
 
-// Scripts
-function js(cb) {
-    gulp.src("src/js/*js")
-        .pipe(babel({
-            presets: ['@babel/preset-env']
-        }))
-        .pipe(concat("script.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/js"));
-    cb();
-}
-
-// Compile Sass
+// SCSS
 function css(cb) {
-    gulp.src("src/scss/*.scss")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    gulp.src("src/scss/**/*.scss")
+        .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
         .pipe(autoprefixer({
             browserlist: ['last 2 versions'],
             cascade: false
@@ -66,19 +48,37 @@ function css(cb) {
     cb();
 }
 
+// JS
+function js(cb) {
+    gulp.src("src/js/**/*js")
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(concat("script.js"))
+        // .pipe(uglify())  // TO MINIMIZE
+        .pipe(gulp.dest("dist/js"));
+    cb();
+}
 
+// IMAGES
+function imageMin(cb) {
+    gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+    cb();
+}
 
-// Watch Files
+// WATCH
 function watch_files() {
     browserSync.init({
         server: {
-            baseDir: "dist/"
+            baseDir: "./dist/"
         }
     });
+    gulp.watch("src/*.html", html).on("change", browserSync.reload);
     gulp.watch("src/scss/**/*.scss", css);
     gulp.watch("src/js/*.js", js).on("change", browserSync.reload);
-    gulp.watch("src/*.html", html).on("change", browserSync.reload);
-
+    gulp.watch("src/**/*.+(png|jpg|jpeg|gif|svg)", imageMin).on("change", browserSync.reload);
 }
 
 // Default 'gulp' command with start local server and watch files for changes.
@@ -86,3 +86,9 @@ exports.default = series(html, css, js, imageMin, watch_files);
 
 // 'gulp build' will build all assets but not run on a local server.
 exports.build = parallel(html, css, js, imageMin);
+
+  //
+  //
+  // ******* END STANDARD SiteBase GULP CONFIG *******
+  //
+  //
